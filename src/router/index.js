bd28807d90a6b1a48f7826df5201ac855cfe13a3
jian zhi qiang <2631095834@qq.com>
2021-02-26 23:36:51 +0800

    前端项目初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Main from "../views/Main";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/main",
+    name: "Main",
+    component: Main
   },
   {
     path: "/about",
@@ -22,6 +28,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 });
 
---
+++ src/views/Main.vue
@@ -0,0 +1,125 @@
+<template>
+    <div>
+        <el-menu
+                :default-active="activeIndex2"
+                class="el-menu-demo"
+                mode="horizontal"
+                @select="handleSelect"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+            <el-menu-item index="1">首页</el-menu-item>
+            <el-menu-item index="2" @click="selectHousingInfo">查询小区信息</el-menu-item>
+        </el-menu>
+        <h1 style="color: blue">租房小区评级系统</h1>
+    <div style="display: flex;">
+        <el-form style="width: 45%" ref="form" :model="form" label-width="180px">
+            <el-form-item label="小区名称">
+                <el-input v-model="form.housingEstate"></el-input>
+            </el-form-item>
+            <el-form-item label="到地铁距离">
+                <el-input v-model="form.subway"></el-input>
+            </el-form-item>
+            <el-form-item label="到公司距离">
+                <el-input v-model="form.company"></el-input>
+            </el-form-item>
+            <el-form-item label="评分">
+                <el-input v-model="form.score"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">记录信息</el-button>
+                <el-button>取消</el-button>
+            </el-form-item>
+        </el-form>
+
+        <div class="block" style="margin-left: 10%">
+            <span class="demonstration">
+                <el-table
+                        :data="tableData"
+                        style="width: 100%;">
+                    <el-table-column
+                            prop="housingEstate"
+                            label="小区名称"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            prop="score"
+                            label="评分"
+                            width="180">
+                    </el-table-column>
+                </el-table>
+            </span>
+            <el-pagination
+                    @current-change="handleCurrentChange"
+                    :current-page.sync="currentPage"
+                    :page-size="total"
+                    layout="total, prev, pager, next"
+                    :total="1000">
+            </el-pagination>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+    export default {
+        name: "Main",
+        data() {
+            return {
+                form: {
+                    housingEstate: '',
+                    subway: '',
+                    company: '',
+                    score: ''
+                },
+                tableData: {
+                    housingEstate: '',
+                    score: ''
+                },
+                currentPage: 1,
+                total: 100,
+                size: 10
+            }
+        },
+        methods: {
+            handleCurrentChange(val) {
+                this.currentPage = val;
+                console.log(`当前页: ${val}`);
+                this.selectHousingInfo();
+            },
+            selectHousingInfo() {
+                let data={
+                    current:this.currentPage,
+                    total:this.total,
+                    size:this.size
+                };
+                axios({
+                    method: 'post',
+                    url: 'http://127.0.0.1:8080/queryHouseInfo',
+                    data: data
+                }).then((res) => {
+                    this.tableData = res.data.data.list;
+                    this.total = res.data.data.total;
+                })
+            },
+            onSubmit() {
+                let data={
+                    housingEstate:this.housingEstate,
+                    subway:this.subway,
+                    company:this.company,
+                    score:this.score
+                };
+                axios({
+                    method: 'post',
+                    url: 'http://127.0.0.1:8080/setHouseInfo',
+                    data: data
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
